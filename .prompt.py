a003ae6010b3f9de3ee301d9374effe6867926bf
Kevin Brown <kbrown@rediker.com>
2014-04-14 23:08:46 -0400

    Redid how the prompt is generated

--- .prompt.py
+++ .prompt.py
@@ -4,30 +4,59 @@ from time import strftime
 import os
 import subprocess
 
+
+def get_terminal_size():
+    """
+    Get the terminal width and height.
+
+    Grabbed from http://stackoverflow.com/a/3010495/359284
+    """
+
+    import fcntl, termios, struct
+
+    h, w, hp, wp = struct.unpack('HHHH',
+        fcntl.ioctl(0, termios.TIOCGWINSZ,
+        struct.pack('HHHH', 0, 0, 0, 0)))
+
+    return w, h
+
+
+def generate_section(text, back_color, text_color,
+                     previous_back_color=None, text_style="NORMAL"):
+    RIGHT_ARROW = u"\u2B80".encode("utf-8")
+
+    back_color = getattr(Back, back_color)
+    text_color = getattr(Fore, text_color)
+    text_style = getattr(Style, text_style)
+
+    section = back_color
+
+    if previous_back_color:
+        fore_color = getattr(Fore, previous_back_color)
+
+        section += fore_color + Style.NORMAL + RIGHT_ARROW
+
+    section += text_color + text_style + " " + text
+
+    return section
+
 time = strftime("%H:%M:%S")
 user = os.environ["USER"]
 cwd = os.getcwd()
 
-RIGHT_ARROW = u"\u2B80".encode("utf-8")
+print generate_section(time, "WHITE", "BLACK"),
+print generate_section(user, "BLUE", "WHITE", "WHITE", "BRIGHT"),
+print generate_section(cwd, "YELLOW", "BLACK", "BLUE", "DIM"),
 
-print Back.WHITE + Fore.BLACK + Style.NORMAL, time,
-print Back.BLUE + Fore.WHITE + RIGHT_ARROW,
-
-print Fore.YELLOW + Style.BRIGHT + user,
-print Fore.BLUE + Style.NORMAL + Back.YELLOW + RIGHT_ARROW,
-
-print Fore.BLACK + Style.DIM + cwd,
-
-INVERT_COLOR = Fore.YELLOW
+last_color = "YELLOW"
 
 if "VIRTUAL_ENV" in os.environ:
     virtualenv = os.environ["VIRTUAL_ENV"]
     virtualenv_name = os.path.basename(virtualenv)
 
-    print Style.NORMAL + INVERT_COLOR + Back.BLUE + RIGHT_ARROW,
-    print Fore.WHITE + Style.BRIGHT + virtualenv_name,
-
-    INVERT_COLOR = Fore.BLUE
+    print generate_section(virtualenv_name, "BLUE", "WHITE", last_color,
+                           "BRIGHT"),
+    last_color = "BLUE"
 
 git_branch = subprocess.Popen(["git", "symbolic-ref", "--short", "HEAD"],
                               stdout=subprocess.PIPE,
@@ -45,31 +74,22 @@ if branch_name:
                                         stdout=subprocess.PIPE)
     needs_committing, _ = staged_changes.communicate()
 
-    OLD_INVERT_COLOR = INVERT_COLOR
-
-    BACK_COLOR = Back.WHITE
-    FORE_COLOR = Fore.BLACK
-    INVERT_COLOR = Fore.WHITE
-    STYLE = Style.DIM
+    back_color = "WHITE"
+    text_color = "BLACK"
+    style = "DIM"
 
     if needs_staging:
-        BACK_COLOR = Back.RED
-        FORE_COLOR = Fore.YELLOW
-        INVERT_COLOR = Fore.RED
-        STYLE = Style.BRIGHT
+        back_color = "RED"
+        text_color = "YELLOW"
+        style = "BRIGHT"
     elif needs_committing:
-        BACK_COLOR = Back.GREEN
-        FORE_COLOR = Fore.WHITE
-        INVERT_COLOR = Fore.GREEN
-        STYLE = Style.BRIGHT
+        back_color = "GREEN"
+        text_color = "WHITE"
+        style = "BRIGHT"
 
-    print (BACK_COLOR + OLD_INVERT_COLOR + Style.NORMAL + RIGHT_ARROW +
-           STYLE + FORE_COLOR),
+    print generate_section(branch_name.strip(), back_color, text_color,
+                           last_color, style),
+    last_color = back_color
 
-    print branch_name.strip(),
-
-print INVERT_COLOR + Back.RESET + Style.NORMAL + RIGHT_ARROW,
-
-print Style.RESET_ALL
-
+print generate_section("", "RESET", "RESET", last_color)
 print Fore.GREEN + Style.BRIGHT + "$ " + Style.RESET_ALL,
